9c56b4055f22e5f2f29442bf043dec19d308b260
VK4041 <151887513+VK4041@users.noreply.github.com>
2026-02-05 17:31:56 +1100

    Finish BST project
    1. Add random array function with range and size in [0,100]
    2. Test all methods via testArray

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 import BSTree from "./tree.js";
 try {
-  const testArray = [1, 1, 2, 9, 8, 4, 3, 2, 6, 7, 5, 5, 5, 5];
+  function getRandomArray() {
+    const randomLength = Math.floor(Math.random() * 100); //Limitnig array size between [0,100] elements
+    const randomArray = [];
+    for (let i = randomLength; i > 0; i--) {
+      randomArray.push(Math.floor(Math.random() * 100)); //Node value between [0,100)
+    }
+    return randomArray;
+  }
+  const displayValue = (value) => console.log(value);
+  const testArray = getRandomArray();
   const tree = new BSTree(testArray);
-  // tree.insert(4.1);
+  tree.insert(400);
+  tree.insert(300);
+  tree.insert(1000);
+  tree.insert(Infinity);
   tree.prettyPrint();
+  tree.isBalanced()
+    ? console.log("Tree is balanced")
+    : console.log("Tree is unbalanced\n");
   tree.reBalance();
-  tree.prettyPrint()
+  tree.prettyPrint();
+  tree.inOrderForEach(displayValue);
 } catch (err) {
   console.log(err);
 }
